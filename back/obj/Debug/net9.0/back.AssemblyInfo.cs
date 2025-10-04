758a065748337491460d000ce876f24a9197add7
Bacarin <henriquebacarin@gmail.com>
2025-10-04 19:29:26 -0300

    passando pra main

--- back/obj/Debug/net9.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net9.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83a79793fbd9e85dbfe191eaee7a36d6f7b03bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07345f5584559986eed5102a29f71f48eea249dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
