e83a79793fbd9e85dbfe191eaee7a36d6f7b03bb
Eduardo Mariano de Oliveira <eduardo.mrn.oliveira@outlook.com>
2025-10-04 12:44:41 -0300

    Restructuring the project

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.help-there',
+  appName: 'Help There',
+  webDir: 'www',
+};
+
+export default config;
